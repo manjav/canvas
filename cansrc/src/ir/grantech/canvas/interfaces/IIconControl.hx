3be32d699ebcf50b0797fe306499ce237b6807f2
Mansour <mansurjavadi@gmail.com>
2020-01-05 01:50:29 +0330

    icon-control interface added

---
+++ cansrc/src/ir/grantech/canvas/interfaces/IIconControl.hx
@@ -0,0 +1,27 @@
+/*
+	GranTech
+	Copyright 2019 Gran Tech. All Rights Reserved.
+
+	This program is free software. You can redistribute and/or modify it in
+	accordance with the terms of the accompanying license agreement.
+ */
+
+ package ir.grantech.canvas.interfaces;
+
+ import openfl.display.BitmapData;
+import feathers.core.IUIControl;
+
+/**
+   A user interface control that displays bitmap data.
+ 
+   @since 1.0.0
+ **/
+ interface IIconControl extends IUIControl {
+   /**
+     The bitmap data to display.
+ 
+     @since 1.0.0
+   **/
+   public var icon(get, set):BitmapData;
+ }
+ 

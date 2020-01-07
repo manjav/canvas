e174bd70b099dc091fc16edfec330db4ed6faaa7
Mansour <mansurjavadi@gmail.com>
2020-01-08 02:32:47 +0330

    canvas added

--- cansrc/src/Main.hx
+++ cansrc/src/Main.hx
@@ -8,6 +8,7 @@ import feathers.layout.VerticalAlign;
 import feathers.style.Theme;
 import ir.grantech.canvas.controls.events.CanEvent;
 import ir.grantech.canvas.controls.groups.Bar;
+import ir.grantech.canvas.controls.groups.CanVas;
 import ir.grantech.canvas.controls.groups.ToolBar;
 import ir.grantech.canvas.themes.CanTheme;
 import openfl.system.Capabilities;
@@ -15,6 +16,7 @@ import openfl.system.Capabilities;
 class Main extends Application {
 	private var left:ToolBar;
 	private var leftExtension:LayoutGroup;
+	private var canvas:CanVas;
 	private var right:Bar;
 
 	public function new() {
@@ -30,6 +32,10 @@ class Main extends Application {
 		this.left.width = Capabilities.screenResolutionX * 0.03;
 		this.left.addEventListener("change", this.left_changeHandler);
 		this.addChild(this.left);
+
+		this.canvas = new CanVas();
+		this.canvas.layoutData = new HorizontalLayoutData(100);
+		this.addChild(this.canvas);
 
 		this.right = new Bar();
 		this.right.width = Capabilities.screenResolutionX * 0.15;

30476c2e9e67c00c6a7556896005501c581bc97a
Mansour <mansurjavadi@gmail.com>
2020-01-09 21:55:53 +0330

    new services added

---
+++ cansrc/src/ir/grantech/services/BaseService.hx
@@ -0,0 +1,16 @@
+package ir.grantech.services;
+
+class BaseService {
+	static private var classes = new Map<String, Dynamic>();
+
+	// static public function set(ng, cl:Class<T>, args:Array<Dynamic> = null):Void {
+	// 	classes.set(cl., Type.createInstance(cl, args));
+	// }
+
+	static public function get(cl:Class<BaseService>, args:Array<Dynamic> = null):Dynamic {
+		var name = Std.string(cl);
+		if (!classes.exists(name))
+			classes.set(name, Type.createInstance(cl, args));
+		return classes.get(name);
+	}
+}

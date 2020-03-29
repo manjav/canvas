d0d3440016bb8f8bdd54aa0a4f4ffbe65448e55c
Mansour <mansurjavadi@gmail.com>
2020-03-29 14:40:58 +0430

    items updated based on input changes

--- cansrc/src/ir/grantech/canvas/controls/groups/panels/TransformPanel.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/panels/TransformPanel.hx
@@ -3,6 +3,8 @@ package ir.grantech.canvas.controls.groups.panels;
 import feathers.controls.CanTextInput;
 import feathers.layout.AnchorLayout;
 import ir.grantech.canvas.themes.CanTheme;
+import openfl.events.Event;
+import openfl.events.FocusEvent;
 
 class TransformPanel extends Panel {
 	private var inputX:CanTextInput;
@@ -20,6 +22,32 @@ class TransformPanel extends Panel {
 		this.inputY = this.createInput(padding * 3, null, null, padding * 1, 1);
 		this.inputW = this.createInput(padding * 1, null, null, padding * 8, 2);
 		this.inputH = this.createInput(padding * 3, null, null, padding * 8, 3);
+	}
+
+	override private function textInputs_focusInHandler(event:FocusEvent):Void {
+		this.inputService.canZoom.scene.transformHint.setVisible(false, true);
+		cast(event.currentTarget, CanTextInput).addEventListener(Event.CHANGE, this.textInputs_changeHandler);
+	}
+
+	override private function textInputs_focusOutHandler(event:FocusEvent):Void {
+		this.inputService.canZoom.scene.transformHint.set(this.inputService.selectedItem);
+		cast(event.currentTarget, CanTextInput).removeEventListener(Event.CHANGE, this.textInputs_changeHandler);
+	}
+
+	private function textInputs_changeHandler(event:Event):Void {
+		if (this.inputService.selectedItem == null)
+			return;
+		var r = this.inputService.selectedItem.rotation;
+		this.inputService.selectedItem.rotation = 0;
+		if (event.currentTarget == this.inputX)
+			this.inputService.selectedItem.x = this.inputX.value;
+		else if (event.currentTarget == this.inputY)
+			this.inputService.selectedItem.y = this.inputY.value;
+		else if (event.currentTarget == this.inputW)
+			this.inputService.selectedItem.width = this.inputW.value;
+		else if (event.currentTarget == this.inputH)
+			this.inputService.selectedItem.height = this.inputH.value;
+		this.inputService.selectedItem.rotation = r;
 	}
 
 	override private function set_enabled(value:Bool):Bool {

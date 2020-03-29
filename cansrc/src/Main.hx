d6b1c1e2d5b31fa6caf47708145a379c49a612d0
Mansour <mansurjavadi@gmail.com>
2020-03-29 22:13:37 +0430

    header added

--- cansrc/src/Main.hx
+++ cansrc/src/Main.hx
@@ -15,10 +15,11 @@ import openfl.display.StageQuality;
 import openfl.display.StageScaleMode;
 
 class Main extends Application {
+	private var zoom:CanZoom;
+	private var header:Panel;
+	private var right:RightBar;
 	private var left:ToolBar;
 	private var leftExtension:Panel;
-	private var zoom:CanZoom;
-	private var right:RightBar;
 	private var zoomLayout:AnchorLayoutData;
 
 	public function new() {
@@ -31,25 +32,33 @@ class Main extends Application {
 		this.layout = new AnchorLayout();
 
 		this.zoom = new CanZoom();
-		this.zoom.layoutData = this.zoomLayout = new AnchorLayoutData(p, 0, p);
+		this.zoom.layoutData = this.zoomLayout = new AnchorLayoutData(0, 0, p);
 		this.addChild(this.zoom);
+
+		this.header = new Panel();
+		this.header.height = CanTheme.DPI * 20;
+		this.header.layoutData = new AnchorLayoutData(p, p, null, p);
+		this.addChild(this.header);
+
+		var h = this.header.height + p * 2;
 
 		this.left = new ToolBar();
 		this.left.width = CanTheme.DPI * 24;
-		this.left.layoutData = new AnchorLayoutData(p, null, p, p);
+		this.left.layoutData = new AnchorLayoutData(h, null, p, p);
 		this.left.addEventListener("change", this.left_changeHandler);
 		this.addChild(this.left);
 
 		this.right = new RightBar();
-		this.right.layoutData = new AnchorLayoutData(p, p, p, null);
+		this.right.layoutData = new AnchorLayoutData(h, p, p, null);
 		this.right.width = CanTheme.DPI * 144;
 		this.addChild(this.right);
 
 		this.leftExtension = new Panel();
-		this.leftExtension.layoutData = new AnchorLayoutData(p, null, p, this.left.width + p * 2);
+		this.leftExtension.layoutData = new AnchorLayoutData(h, null, p, this.left.width + p * 2);
 		this.leftExtension.width = CanTheme.DPI * 120;
 
-		this.zoomLayout.right = this.right.width;
+		this.zoomLayout.top = h;
+		this.zoomLayout.right = this.right.width + p;
 		this.zoomLayout.left = this.left.width + p * 2;
 		// stage.addEventListener(Event.RESIZE, this.stage_resizeHandler);
 	}

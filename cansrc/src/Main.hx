e4f43bf6bf0a44d3c9b4643e85af89fc7fc7ad64
Mansour <mansurjavadi@gmail.com>
2020-03-29 14:12:56 +0430

    new layout adjustment for main

--- cansrc/src/Main.hx
+++ cansrc/src/Main.hx
@@ -1,13 +1,13 @@
 package;
 
 import feathers.controls.Application;
-import feathers.layout.HorizontalLayout;
-import feathers.layout.HorizontalLayoutData;
+import feathers.layout.AnchorLayout;
+import feathers.layout.AnchorLayoutData;
 import feathers.layout.VerticalAlign;
 import feathers.style.Theme;
 import ir.grantech.canvas.controls.events.CanEvent;
-import ir.grantech.canvas.controls.groups.Panel;
 import ir.grantech.canvas.controls.groups.CanZoom;
+import ir.grantech.canvas.controls.groups.Panel;
 import ir.grantech.canvas.controls.groups.RightBar;
 import ir.grantech.canvas.controls.groups.ToolBar;
 import ir.grantech.canvas.themes.CanTheme;
@@ -19,6 +19,7 @@ class Main extends Application {
 	private var leftExtension:Panel;
 	private var zoom:CanZoom;
 	private var right:RightBar;
+	private var zoomLayout:AnchorLayoutData;
 
 	public function new() {
 		stage.quality = StageQuality.BEST;
@@ -26,38 +27,41 @@ class Main extends Application {
 		Theme.setTheme(new CanTheme());
 		super();
 
-		// BaseService.get(KeyBoardService, [stage]);
-		var layout = new HorizontalLayout();
-		layout.verticalAlign = VerticalAlign.JUSTIFY;
-		layout.gap = layout.paddingTop = CanTheme.DEFAULT_PADDING;
-		this.layout = layout;
+		var p = CanTheme.DEFAULT_PADDING;
+		this.layout = new AnchorLayout();
+
+		this.zoom = new CanZoom();
+		this.zoom.layoutData = this.zoomLayout = new AnchorLayoutData(p, 0, p);
+		this.addChild(this.zoom);
 
 		this.left = new ToolBar();
 		this.left.width = CanTheme.DPI * 24;
+		this.left.layoutData = new AnchorLayoutData(p, null, p, p);
 		this.left.addEventListener("change", this.left_changeHandler);
 		this.addChild(this.left);
 
-		this.zoom = new CanZoom();
-		this.zoom.layoutData = new HorizontalLayoutData(100);
-		this.addChild(this.zoom);
-
 		this.right = new RightBar();
+		this.right.layoutData = new AnchorLayoutData(p, p, p, null);
 		this.right.width = CanTheme.DPI * 144;
 		this.addChild(this.right);
 
 		this.leftExtension = new Panel();
+		this.leftExtension.layoutData = new AnchorLayoutData(p, null, p, this.left.width + p * 2);
 		this.leftExtension.width = CanTheme.DPI * 120;
 
+		this.zoomLayout.right = this.right.width;
+		this.zoomLayout.left = this.left.width + p * 2;
 		// stage.addEventListener(Event.RESIZE, this.stage_resizeHandler);
 	}
 
 	private function left_changeHandler(event:CanEvent):Void {
+		if (event.data.index == -1) {
 		if (this.leftExtension != null && this.leftExtension.parent == this)
 			this.removeChild(this.leftExtension);
-
-		if (event.data.index == -1)
-			return;
-
-		this.addChildAt(this.leftExtension, 0);
+			this.zoomLayout.left = this.left.width + CanTheme.DEFAULT_PADDING * 2;
+		} else {
+			this.addChild(this.leftExtension);
+			this.zoomLayout.left = this.left.width + this.leftExtension.width + CanTheme.DEFAULT_PADDING * 3;
+		}
 	}
 }

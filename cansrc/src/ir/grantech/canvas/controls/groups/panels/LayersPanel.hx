a7605c25c92eb9e989cd3ec462a2aed2f7ad9967
Mansour <mansurjavadi@gmail.com>
2020-04-01 22:36:36 +0430

    bind layer-data to view

--- cansrc/src/ir/grantech/canvas/controls/groups/panels/LayersPanel.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/panels/LayersPanel.hx
@@ -4,6 +4,7 @@ import feathers.controls.ListView;
 import feathers.layout.AnchorLayoutData;
 import feathers.utils.DisplayObjectRecycler;
 import ir.grantech.canvas.controls.items.LayerItemRenderer;
+import ir.grantech.services.LayersService.Layer;
 import openfl.events.Event;
 
 class LayersPanel extends ListPanel {
@@ -17,5 +18,20 @@ class LayersPanel extends ListPanel {
 			return item.name;
 		};
 	}
+
+	override private function layoutGroup_addedToStageHandler(event:Event):Void {
+		super.layoutGroup_addedToStageHandler(event);
+		this.layersService.addEventListener(Event.CHANGE, layersService_changeHandler);
+		this.listView.dataProvider = this.layersService.items;
+	}
+
+	override private function layoutGroup_removedFromStageHandler(event:Event):Void {
+		super.layoutGroup_removedFromStageHandler(event);
+		this.layersService.removeEventListener(Event.CHANGE, layersService_changeHandler);
+		this.listView.dataProvider = null;
+	}
+
+	private function layersService_changeHandler(event:Event):Void {}
+
 	override private function listView_changeHandler(event:Event):Void {}
 }

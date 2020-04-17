e9bc547746b0679f84df8666d6a2efaee6218275
Mansour <mansurjavadi@gmail.com>
2020-04-18 01:16:57 +0430

    set text target

--- cansrc/src/ir/grantech/canvas/controls/groups/panels/TextPanel.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/panels/TextPanel.hx
@@ -1,14 +1,18 @@
 package ir.grantech.canvas.controls.groups.panels;
 
 import ir.grantech.canvas.drawables.CanItems;
+import ir.grantech.canvas.drawables.CanText;
 import ir.grantech.canvas.services.Layers.Layer;
 
 class TextPanel extends Panel {
+	private var target:CanText;
+
 	override private function set_targets(value:CanItems):CanItems {
 		super.set_targets(value);
 		var isText = value.length == 1 && value.get(0).layer.type == Layer.TYPE_TEXT;
 		this.includeInLayout = isText;
 		this.visible = isText;
+		this.target = isText ? cast(value.get(0), CanText) : null;
 		return value;
 	}
 

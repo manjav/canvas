86f953d6abdb90ae451c9171ef9e09e660444f26
Mansour <mansurjavadi@gmail.com>
2020-04-18 01:15:12 +0430

    font family categorized

--- cansrc/src/ir/grantech/canvas/services/Fonts.hx
+++ cansrc/src/ir/grantech/canvas/services/Fonts.hx
@@ -18,5 +18,38 @@ class Fonts {
 				fonts.push(Font.fromFile(fontsPath + f));
 		#end
 
+		var ret = new Array<FontFamily>();
+		var family:FontFamily = new FontFamily(fonts[0]);
+		ret.push(family);
+		for (i in 1...fonts.length) {
+			if (fonts[i].fontName.indexOf(family.name) > -1) {
+				family.addStyle(fonts[i], fonts[i].fontName.substr(family.name.length + 1));
+				continue;
+			}
+			family = new FontFamily(fonts[i]);
+			ret.push(family);
+		}
+		return ret;
 	}
+}
+
+// Typesettg Bold Italic Black Fixed Semibold Semilight Light Emoji Negreta Cursiva Historic
+
+class FontFamily {
+
+	public var name:String;
+	public var styles:Array<Font>;
+	public var styleNames:Array<String>;
+
+	public function new(font:Font) {
+		this.name = font.fontName;
+		this.styles = new Array<Font>();
+    this.styleNames = new Array<String>();
+    this.addStyle(font, "Regular");
+  }
+  
+  public function addStyle(font:Font, name:String):Void{
+		this.styles.push(font);
+    this.styleNames.push(name); 
+  }
 }

7f18e40b9dda3c7860695a6055e114303a930c86
Mansour <mansurjavadi@gmail.com>
2020-04-18 01:14:52 +0430

    get fonts for non flash platforms

--- cansrc/src/ir/grantech/canvas/services/Fonts.hx
+++ cansrc/src/ir/grantech/canvas/services/Fonts.hx
@@ -6,5 +6,17 @@ import openfl.text.Font;
 class Fonts {
 	static public function load():Array<FontFamily> {
 		var fonts:Array<Font> = Font.enumerateFonts(true);
+		#if !(flash || html5)
+		fonts = new Array<Font>();
+		var fontsPath = lime.system.System.fontsDirectory.toLowerCase();
+		var index = fontsPath.lastIndexOf("fonts");
+		if (fontsPath.substr(index) != "fonts\\")
+			fontsPath = fontsPath.substr(0, index) + "fonts\\";
+		var dir = sys.FileSystem.readDirectory(fontsPath);
+		for (f in dir)
+			if (Utils.getExtension(f) == "ttf")
+				fonts.push(Font.fromFile(fontsPath + f));
+		#end
+
 	}
 }

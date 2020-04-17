573912311df2edfc33e1057604302bce5fe32fbf
Mansour <mansurjavadi@gmail.com>
2020-04-18 02:21:45 +0430

    font-face replaced with fontstyle

--- cansrc/src/ir/grantech/canvas/services/Fonts.hx
+++ cansrc/src/ir/grantech/canvas/services/Fonts.hx
@@ -22,7 +22,7 @@ class Fonts {
 		ret.push(family);
 		for (i in 1...fonts.length) {
 			if (fonts[i].fontName.indexOf(family.name) > -1) {
-				family.addStyle(fonts[i], fonts[i].fontName.substr(family.name.length + 1));
+				family.styles.push(new FontStyle(fonts[i], family, fonts[i].fontName.substr(family.name.length + 1)));
 				continue;
 			}
 			family = new FontFamily(fonts[i]);
@@ -36,18 +36,40 @@ class Fonts {
 
 class FontFamily {
 	public var name:String;
-	public var styles:Array<Font>;
-	public var styleNames:Array<String>;
+	public var styles:Array<FontStyle>;
 
 	public function new(font:Font) {
 		this.name = font.fontName;
-		this.styles = new Array<Font>();
-    this.styleNames = new Array<String>();
-    this.addStyle(font, "Regular");
-  }
-  
-  public function addStyle(font:Font, name:String):Void{
-		this.styles.push(font);
-    this.styleNames.push(name); 
-  }
+		this.styles = new Array<FontStyle>();
+		this.styles.push(new FontStyle(font, this, "Regular"));
+	}
+
+	static public function findByStyle(families:Array<FontFamily>, style:String):FontStyle {
+		for (f in families)
+			for (s in f.styles)
+				if (s.font.fontName == style)
+					return s;
+		return null;
+	}
+}
+
+class FontStyle {
+	public var font:Font;
+	public var family:FontFamily;
+	public var styleName:String;
+
+	public var fontName(get, never):String;
+	private function get_fontName():String {
+		#if flash
+		return font.fontName;
+		#else
+		return font.name;
+		#end
+	}
+
+	public function new(font:Font, family:FontFamily, styleName:String) {
+		this.font = font;
+		this.family = family;
+		this.styleName = styleName;
+	}
 }

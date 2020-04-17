80d82b40efa4bb519530bd46e5ea7e4e1052e94f
Mansour <mansurjavadi@gmail.com>
2020-04-18 02:23:26 +0430

    text-panel update on selection

--- cansrc/src/ir/grantech/canvas/controls/groups/panels/TextPanel.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/panels/TextPanel.hx
@@ -15,23 +15,25 @@ class TextPanel extends Panel {
 	private var target:CanText;
 
 	override private function set_targets(value:CanItems):CanItems {
-		super.set_targets(value);
 		var isText = value.length == 1 && value.get(0).layer.type == Layer.TYPE_TEXT;
 		this.includeInLayout = isText;
 		this.visible = isText;
 		this.target = isText ? cast(value.get(0), CanText) : null;
+		super.set_targets(value);
 		return value;
 	}
 
+	private var families:Array<FontFamily>;
 	private var familyList:ComboBox;
 	override private function initialize() {
 		super.initialize();
 		this.layout = new AnchorLayout();
 
 		this.title = "TEXT";
+		this.families = Fonts.load();
 
 		// font families
-		this.familyList = this.createComboBox(Fonts.load(), new AnchorLayoutData(padding * 3, padding, null, padding));
+		this.familyList = this.createComboBox(this.families, new AnchorLayoutData(padding * 3, padding, null, padding));
 		this.familyList.itemToText = (family:FontFamily) -> {
 			return family.name;
 		};
@@ -40,17 +42,32 @@ class TextPanel extends Panel {
 	}
 
 	override private function popupListView_changeHandler(event:Event):Void {
-		if (cast(event.currentTarget, ComboBox).selectedIndex == -1)
+		if (this.updating || cast(event.currentTarget, ComboBox).selectedIndex == -1)
 			return;
 			var family = cast(this.familyList.selectedItem, FontFamily);
+		if (event.currentTarget == this.familyList) {
 			this.changeFont(family.styles[0]);
-			this.styleList.dataProvider = new ArrayCollection(family.styleNames);
+			this.updating = true;
+			this.styleList.dataProvider = new ArrayCollection(family.styles);
+			this.updating = false;
 	}
 
-	function changeFont(font:Font):Void {
-		Font.registerFont(font);
+	function changeFont(font:FontStyle):Void {
+		Font.registerFont(font.font);
 		var textFormat = this.target.getTextFormat();
 		textFormat.font = font.fontName;
 		this.target.setTextFormat(textFormat);
+	}
+
+	override public function updateData():Void {
+		if (this.target == null || !this.targets.filled)
+			return;
+		this.updating = true;
+
+		var textFormat = this.target.getTextFormat();
+		var style:FontStyle = FontFamily.findByStyle(this.families, textFormat.font);
+		this.familyList.selectedItem = style.family;
+
+		this.updating = false;
 	}
 }

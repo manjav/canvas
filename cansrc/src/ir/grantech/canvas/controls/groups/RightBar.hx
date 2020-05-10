a56ddb2bbc2886828c87b3c2f1fc245903360a35
Mansour <mansurjavadi@gmail.com>
2020-05-10 16:15:17 +0430

    detect point any way

--- cansrc/src/ir/grantech/canvas/controls/groups/RightBar.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/RightBar.hx
@@ -55,8 +55,6 @@ class RightBar extends ScrollContainer {
 	}
 
 	private function input_pointHandler(event:Event):Void {
-		if (Tools.instance.toolType != Tool.SELECT)
-			return;
 		if (Inputs.instance.pointPhase < Inputs.PHASE_ENDED)
 			this.transfromSection.updateData();
 	}

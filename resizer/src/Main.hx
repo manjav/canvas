a276b1a1cc289f743215a17901d01b705cf9f841
Mansour <mansurjavadi@gmail.com>
2020-05-11 20:51:43 +0430

    batch resize images

--- resizer/src/Main.hx
+++ resizer/src/Main.hx
@@ -1,18 +1,27 @@
-import haxe.Timer;
+import bitmap.Bitmap;
 import bitmap.IOUtil;
 import bitmap.PNGBitmap;
 import bitmap.transformation.Affine;
+import haxe.Timer;
+import sys.FileSystem;
+import sys.io.File;
 
 class Main {
 	static function main() {
 		var t = Timer.stamp();
 
-		var bitmap = PNGBitmap.create(IOUtil.readFile("input.png"));
-		scale(bitmap, 0.9, 0.9);
-		
+		var srcDir = "origin";
+		var dstDir = "C:/_projects/_canvas/cansrc/assets/img/";
+		for (f in FileSystem.readDirectory(srcDir)) {
+			var path = haxe.io.Path.join([srcDir, f]);
+			var bitmap = PNGBitmap.create(IOUtil.readFile(path));
+			for (i in 1...5) {
+				IOUtil.writeBitmap(haxe.io.Path.join([dstDir + i + "x", f]), scale(bitmap, i / 4, i / 4));
+			}
+		}
 		trace(Timer.stamp() - t, "Hello, world!");
 	}
-	
+
 	static function scale(bitmap:PNGBitmap, sx:Float, sy:Float):Bitmap {
 		if (sx == 1 && sy == 1)
 			return bitmap;

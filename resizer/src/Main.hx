53adbc128dfba041de8362c19d96afc70df7f28e
Mansour <mansurjavadi@gmail.com>
2020-05-11 20:03:13 +0430

    sample resize code added

--- resizer/src/Main.hx
+++ resizer/src/Main.hx
@@ -5,4 +5,19 @@ import bitmap.transformation.Affine;
 
 class Main {
 	static function main() {
+		var t = Timer.stamp();
+
+		var bitmap = PNGBitmap.create(IOUtil.readFile("input.png"));
+		var r = bitmap.transform.affine({affine: new Affine().scale(sx, sy)});
+		var w = Math.floor(bitmap.width * sx);
+		var h = Math.floor(bitmap.height * sy);
+		var b = new PNGBitmap(w, h);
+		b.copyFrom(r.bitmap, {x: 0, y: 0}, {
+			x: 0,
+			y: 0,
+			width: w,
+			height: h
+		});
+		IOUtil.writeBitmap('output.png', b);
+	}
 }

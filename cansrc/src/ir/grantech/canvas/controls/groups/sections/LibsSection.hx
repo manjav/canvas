3d70071a66654725592aab2d4bc25607ff9c4dc5
Mansour <mansurjavadi@gmail.com>
2020-06-11 17:44:45 +0430

    lib-item-renderer first step

--- cansrc/src/ir/grantech/canvas/controls/groups/sections/LibsSection.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/sections/LibsSection.hx
@@ -1,5 +1,7 @@
 package ir.grantech.canvas.controls.groups.sections;
 
+import ir.grantech.canvas.controls.items.LibItemRenderer;
+import ir.grantech.canvas.services.Libs.LibItem;
 import ir.grantech.canvas.services.Layers.Layer;
 import ir.grantech.canvas.services.Commands;
 import ir.grantech.canvas.controls.items.LayerItemRenderer;
@@ -8,15 +10,14 @@ import feathers.utils.DisplayObjectRecycler;
 class LibsSection extends ListSection {
 	override private function initialize() {
 		this.title = "ASSETS";
-		this.data = this.commands.layers;
-		this.itemRendererRecycler = DisplayObjectRecycler.withClass(LayerItemRenderer);
-		
+		this.data = this.libs.items;
+		this.itemRendererRecycler = DisplayObjectRecycler.withClass(LibItemRenderer);
+
 		super.initialize();
 
-		this.listView.itemToText = (item:Layer) -> {
-			return item.getString(Commands.NAME);
+		this.listView.itemToText = (item:LibItem) -> {
+			return item.name;
 		};
 		// this.listView.addEventListener(CanEvent.ITEM_SELECT, this.listView_itemSelectHandler);
 	}
-
 }
---
+++ cansrc/src/ir/grantech/canvas/controls/items/LibItemRenderer.hx
@@ -0,0 +1,69 @@
+package ir.grantech.canvas.controls.items;
+
+import feathers.controls.ToggleButtonState;
+import feathers.controls.dataRenderers.IDataRenderer;
+import feathers.controls.dataRenderers.ItemRenderer;
+import feathers.skins.RectangleSkin;
+import feathers.style.Theme;
+import ir.grantech.canvas.services.Libs.LibItem;
+import ir.grantech.canvas.themes.CanTheme;
+import ir.grantech.canvas.themes.ScaledBitmap;
+
+class LibItemRenderer extends ItemRenderer implements IDataRenderer {
+	@:isVar
+	public var data(get, set):Dynamic;
+
+	private function set_data(value:Dynamic):Dynamic {
+		if (this.data == value)
+			return this.data;
+		if (value == null)
+			return value;
+		this.data = value;
+		this.item = cast(this.data, LibItem);
+		return this.data;
+	}
+
+	private function get_data():Dynamic {
+		return this.data;
+	}
+
+	private var item:LibItem;
+
+	public function new() {
+		super();
+		this.height = Math.round(CanTheme.CONTROL_SIZE);
+	}
+
+	override private function initializeItemRendererTheme():Void {}
+
+	@:access(ir.grantech.canvas.themes.CanTheme)
+	override function initialize():Void {
+		super.initialize();
+		var theme = Std.downcast(Theme.getTheme(), CanTheme);
+		var skin = new RectangleSkin();
+		skin.fill = SolidColor(theme.controlFillColor1);
+		skin.selectedFill = SolidColor(theme.dividerColor);
+		skin.setFillForState(ToggleButtonState.HOVER(false), SolidColor(theme.dividerColor, 0.2));
+		this.backgroundSkin = skin;
+
+		this.selectedTextFormat = this.textFormat;
+		this.setTextFormatForState(ToggleButtonState.DOWN(false), this.textFormat);
+
+		this.icon = new ScaledBitmap("bitmap");
+
+		this.typeField = new TextField();
+		this.addChild(this.typeField);
+
+		this.iconPosition = MANUAL;
+		this.gap = CanTheme.DEFAULT_PADDING;
+	}
+
+	override private function layoutContent():Void {
+		this.icon.x = this.paddingLeft;
+		this.icon.y = (this.actualHeight - this.icon.height) * 0.5;
+
+		this.textField.x = this.paddingLeft + this.icon.width + this.gap;
+		this.textField.y = (this.actualHeight - this.textField.height) * 0.5;
+
+	}
+}

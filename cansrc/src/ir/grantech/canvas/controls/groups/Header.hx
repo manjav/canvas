632e5e05ee67df872c107fe8844bb785d398f45d
Mansour <mansurjavadi@gmail.com>
2020-07-02 08:26:20 +0430

    Add Restore Button

--- cansrc/src/ir/grantech/canvas/controls/groups/Header.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/Header.hx
@@ -14,6 +14,7 @@ class Header extends CanSection {
 	private var closeButton:Button;
 	private var maximizeButton:Button;
 	private var minimizeButton:Button;
+	private var restoreButton:Button;
 
 	override private function initialize() {
 		super.initialize();
@@ -25,6 +26,8 @@ class Header extends CanSection {
 		closeButton = this.createButton("header-close", AnchorLayoutData.middleRight(0, 3 * CanTheme.DPI));
 		maximizeButton = this.createButton("header-maximize", AnchorLayoutData.middleRight(0, 24 * CanTheme.DPI));
 		minimizeButton = this.createButton("header-minimize", AnchorLayoutData.middleRight(0, 48 * CanTheme.DPI));
+		restoreButton = this.createButton("header-restore", AnchorLayoutData.middleRight(0, 24 * CanTheme.DPI));
+		restoreButton.visible = false;
 	}
 
 	override private function buttons_clickHandler(event:MouseEvent):Void {
@@ -36,6 +39,8 @@ class Header extends CanSection {
 			this.maximize();
 		else if (event.currentTarget == minimizeButton)
 			this.minimize();
+		else if (event.currentTarget == restoreButton)
+			this.restore();
 	}
 
 	private function maximize():Void {
@@ -44,6 +49,10 @@ class Header extends CanSection {
 		this.stage.window.maximized = true;
 	}
 
+	private function restore():Void {
+		this.restoreButton.visible = false;
+		this.maximizeButton.visible = true;
+		this.stage.window.maximized = false;
 	}
 
 	private function minimize():Void {

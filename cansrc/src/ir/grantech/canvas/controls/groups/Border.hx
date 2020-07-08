8798f57a1c4dbd0ef1b7a3729f838d9d8ebf290d
Mansour <mansurjavadi@gmail.com>
2020-07-08 16:47:36 +0430

    Add mouse handlers

--- cansrc/src/ir/grantech/canvas/controls/groups/Border.hx
+++ cansrc/src/ir/grantech/canvas/controls/groups/Border.hx
@@ -30,5 +30,53 @@ class Border extends LayoutGroup {
 		skin.graphics.drawRect(0, B, B, stage.stageHeight - B * 2);
 		skin.graphics.endFill();
 		this.backgroundSkin = skin;
+
+		this.addEventListener(MouseEvent.MOUSE_DOWN, this.borders_mouseDownHandler);
+		this.addEventListener(MouseEvent.MOUSE_MOVE, this.borders_mouseMoveHandler);
+		this.addEventListener(MouseEvent.MOUSE_OUT, this.borders_mouseOutHandler);
+	}
+
+	private function borders_mouseMoveHandler(event:MouseEvent):Void {
+		event.updateAfterEvent();
+		if (event.stageY < B) {
+			if (event.stageX < B)
+				this.side = 0;
+			else if (event.stageX > stage.stageWidth - B)
+				this.side = 2;
+			else
+				this.side = 1;
+		} else if (event.stageY > stage.stageHeight - B) {
+			if (event.stageX < B)
+				this.side = 6;
+			else if (event.stageX > stage.stageWidth - B)
+				this.side = 8;
+			else
+				this.side = 7;
+		} else {
+			if (event.stageX < B)
+				this.side = 3;
+			else if (event.stageX > stage.stageWidth - B)
+				this.side = 5;
+			else
+				this.side = 4;
+		}
+	}
+
+	private function borders_mouseOutHandler(event:MouseEvent):Void {
+	}
+
+	private function borders_mouseDownHandler(event:MouseEvent):Void {
+		this.removeEventListener(MouseEvent.MOUSE_OUT, this.borders_mouseOutHandler);
+		this.removeEventListener(MouseEvent.MOUSE_MOVE, this.borders_mouseMoveHandler);
+		this.removeEventListener(MouseEvent.MOUSE_DOWN, this.borders_mouseDownHandler);
+		stage.addEventListener(MouseEvent.MOUSE_UP, this.borders_mouseUpHandler);
+		stage.window.onMouseMoveRelative.add(windowMouseMove);
+	}
+
+	private function borders_mouseUpHandler(event:MouseEvent):Void {
+		this.addEventListener(MouseEvent.MOUSE_OUT, this.borders_mouseOutHandler);
+		this.addEventListener(MouseEvent.MOUSE_MOVE, this.borders_mouseMoveHandler);
+		this.addEventListener(MouseEvent.MOUSE_DOWN, this.borders_mouseDownHandler);
+		stage.removeEventListener(MouseEvent.MOUSE_UP, this.borders_mouseUpHandler);
 	}
 }
